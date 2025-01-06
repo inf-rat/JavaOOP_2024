49562ec36a18d0f4873a904b0bff652091485815
inf-rat <r.ratke@schule.bremen.de>
2025-01-06 11:21:19 +0100

    AB 05, Aufg. 3b)

--- Grundlagen/AB05_ModuloArrays/src/VierGewinnt.java
+++ Grundlagen/AB05_ModuloArrays/src/VierGewinnt.java
@@ -7,12 +7,18 @@ public class VierGewinnt {
         for (int i = 0; i < names.length; i++) {
             System.out.println("Name von Spieler " + (i + 1) + ": ");
             names[i] = scObj.nextLine();
-            while (names[i].length() < 3) {
+            while (names[i].isEmpty()) {
                 System.out.println("Name ungültig. Bitte erneut versuchen:");
                 names[i] = scObj.nextLine();
             }
             System.out.println("Spieler " + (i + 1) + ": " + names[i]);
         }
+        int[] anzahlSteine = new int[7];
+        System.out.println("Anzahl der Steine im Spielfeld:");
+        for (int anzahl: anzahlSteine) {
+            System.out.print(anzahl + " ");
+        }
+
 
     }
 }
