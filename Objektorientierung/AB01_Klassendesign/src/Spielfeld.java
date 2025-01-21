a56ec0ef5cc6175cdaa60566e7b584e5b4187de0
inf-rat <r.ratke@schule.bremen.de>
2025-01-21 10:42:21 +0100

    AB 06, Aufg. 2

---
+++ Objektorientierung/AB01_Klassendesign/src/Spielfeld.java
@@ -0,0 +1,20 @@
+public class Spielfeld {
+    // Konstanten
+    private static final byte SPALTEN = 7;
+    private static final byte ZEILEN = 6;
+    private static final char LEERES_SYMBOL = '◌';
+    private static final char SYMBOL_1 = '●';
+    private static final char SYMBOL_2 = '○';
+
+    // Attribute
+    private char[][] spielsteine = new char[ZEILEN][SPALTEN];
+
+    // Konstruktor
+    public Spielfeld() {
+        for (int zeile = 0; zeile < ZEILEN; zeile++) {
+            for (int spalte = 0; spalte < SPALTEN; spalte++) {
+                spielsteine[zeile][spalte] = LEERES_SYMBOL;
+            }
+        }
+    }
+}
